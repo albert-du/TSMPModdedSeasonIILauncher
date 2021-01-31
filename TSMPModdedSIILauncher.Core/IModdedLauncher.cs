810f5dcfbd38e710818d3b6682f1df6df3ba7b2a
albert-du <52804499+albert-du@users.noreply.github.com>
2021-01-30 16:57:19 -0800

    Add images for wiki

---
+++ TSMPModdedSIILauncher.Core/IModdedLauncher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSMPModdedSIILauncher.Core
+{
+    interface IModdedLauncher
+    {
+        public bool VersionCheckInProgress { get; protected set; }
+
+        public bool DownloadInProgress { get; protected set; }
+
+        public bool InstallInProgress { get; protected set; }
+        public Configuration Configuration { get; protected set; }
+        public ModpackService ModpackService { get; protected set; }
+        public Launcher Launcher { get; protected set; }
+        public Shell Shell { get; protected set; }
+        public OptifineService optifineService { get; protected set; }
+
+        public void Start();
+
+        public void Initialize()
+        {
+
+        }
+    }
+}
